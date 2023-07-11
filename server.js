17e768b6cac54f86b58e7ac9675a2d8d0681d39a
Ankit <ankitsuneja456.com>
2023-07-11 15:03:48 +0530

    contact

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv= require("dotenv").config()
+
+const app = express()
+
+const port = process.env.port;
+
+app.listen(port, ()=>{
+    console.log(`connected to ${port}` );
+})
+
