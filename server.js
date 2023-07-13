c5828fc0bfe01b72534bcadac162d5c23a33c68b
Ankit <ankitsuneja456.com>
2023-07-13 16:55:02 +0530

    commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express()
 
 const port = process.env.port;
 
+app.get("/api/contacts", (req, res, next)=>{
+    res.status(200).json({message:"hello"})
+})
+
 app.listen(port, ()=>{
     console.log(`connected to ${port}` );
 })
