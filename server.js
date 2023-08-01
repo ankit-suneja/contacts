b5d67af727594d35f6680827de320567d47cf80f
Ankit <ankitsuneja456.com>
2023-08-01 17:48:49 +0530

    mongo

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const dotenv= require("dotenv").config()
-const router = require("./routes/Contactroutes")
+const router = require("./routes/Contactroutes");
+const errorHandler = require("./routes/Controllers/middleware/errorhandler");
 
 const app = express()
 
 const port = process.env.port;
 
 app.use(express.json());
-
 app.use("/api/contacts", router); 
+app.use(errorHandler);
 
-app.listen(port, ()=>{
-    console.log(`connected to ${port}` );
-})
-
+mongoose.connect('mongodb+srv://admin:RwMZOP2HhrrHNmt9@cluster0.ghyejse.mongodb.net/')
+    .then(()=>app.listen(port))
+    .then(()=>console.log(`connected to database and listening on port ${port}`))
+    .catch((err)=>console.log(err))
+ 
